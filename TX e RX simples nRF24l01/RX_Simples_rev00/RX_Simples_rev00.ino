f327cd459c1f73f82cee0d7a8436cbcb46cb348b
Pedro Cândido do Nascimento Filho <pedrocnf@gmail.com>
2021-02-18 22:04:26 -0300

    Add files via upload

---
+++ TX e RX simples nRF24l01/RX_Simples_rev00/RX_Simples_rev00.ino
@@ -0,0 +1,41 @@
+/* PROGRANA PADRÃO DE ACIONAMENTO DO nRF24
+ * RECEPTOR
+ * Conexões Pinnos CE: 9, CSN: 10, SCK: 13, MO: 11, MI: 12
+ * (Se ligar direto no Arduino usar VCC 3.3v, Se usar adaptador ligar no 5v)
+ * 
+ * Autor: Pedro Cândido do NAscimento Filho
+ */
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+int dados[2]; //Vetor de dados a trasmitir, pode ser mudado o tamanho
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; //Pipe de comunicação
+
+RF24 radio(9,10); //Configuração do CE e do CSN
+
+void setup()
+{
+  Serial.begin(9600);
+  radio.begin();
+  //radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+}
+
+void loop()
+{
+  if ( radio.available() )
+  {
+      radio.read(dados, sizeof(dados) );
+      Serial.println(dados[0]);
+      Serial.println(dados[1]); 
+  }
+  else
+  {
+    Serial.println("Rádio não disponível");
+  }
+  delay(1000);
+}
---
+++ TX e RX simples nRF24l01/TX_Simples_rev00/TX_Simples_rev00.ino8790528340274070202.tmp
@@ -0,0 +1,35 @@
+/* PROGRANA PADRÃO DE ACIONAMENTO DO nRF24
+ * TRANSMISSOR
+ * Conexões Pinnos CE: 9, CSN: 10, SCK: 13, MO: 11, MI: 12
+ * (Se ligar direto no Arduino usar VCC 3.3v, Se usar adaptador ligar no 5v)
+ * 
+ * Autor: Pedro Cândido do NAscimento Filho
+ */
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+int dados[2];
+
+RF24 radio(9,10);
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  radio.begin();
+  //radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(pipe);
+}
+
+void loop()
+{
+  dados[0] = 6;
+  dados[1] = 1;
+  
+  radio.write(dados, sizeof(dados) );
+
+}
+
